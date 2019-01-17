9a9923266ac03d36b998f26cef7374e047395b96
egorkqq <egorkqq@gmail.com>
2019-01-17 22:25:18 +0500

    feat: calendar component with ability to update month/year/etc

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React, { Component } from "react";
+import Calendar from "./Calendar";
+import moment from "moment";
+import "moment/locale/ru";
+import cx from "classnames";
+export default class App extends Component {
+  state = {
+    date: moment()
+  };
+  setToday = () => {
+    this.setState({ date: moment() });
+  };
   render() {
+    let weekdayName = 7;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Calendar
+        onChangeMonth={date => this.setState({ date })}
+        date={this.state.date}
+        onPickDate={date => console.log(date.startOf("month"))}
+        renderDay={({ day, classNames, onPickDate }) => {
+          while (weekdayName) {
+            weekdayName--;
+            return (
+              <div
+                key={day.format()}
+                className={cx(
+                  "Calendar-grid-item",
+                  day.isSame(moment(), "day") && "Calendar-grid-item--current",
+                  classNames
+                )}
+                onClick={e => onPickDate(day)}
+              >
+                {day
+                  .format("dddd")
+                  .charAt(0)
+                  .toUpperCase() + day.format("dddd").slice(1)}
+                {",  "}
+                {day.format("D")}
+              </div>
+            );
+          }
+          return (
+            <div
+              key={day.format()}
+              className={cx(
+                "Calendar-grid-item",
+                day.isSame(moment(), "day") && "Calendar-grid-item--current",
+                classNames
+              )}
+              onClick={e => onPickDate(day)}
+            >
+              {day.format("D")}
+            </div>
+          );
+        }}
+        renderHeader={({ date, onPrevMonth, onNextMonth }) => (
+          <div className="Calendar-header">
+            <button onClick={onPrevMonth}>«</button>
+            <div className="Calendar-header-currentDate">
+              {date
+                .format("MMMM YYYY")
+                .charAt(0)
+                .toUpperCase() + date.format("MMMM YYYY").slice(1)}
+            </div>
+            <button onClick={onNextMonth}>»</button>
+            <button onClick={this.setToday}>Сегодня</button>
+          </div>
+        )}
+      />
     );
   }
 }
-
-export default App;
---
+++ src/Calendar/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import cx from "classnames";
+import createDateObjects from "./createDateObjects";
+import "./styles.sass";
+
+export default class Calendar extends Component {
+  handleNextMonth = () => {
+    if (this.props.onNextMonth) {
+      return this.props.onNextMonth();
+    }
+
+    this.props.onChangeMonth(this.props.date.clone().add(1, "months"));
+  };
+
+  handlePrevMonth = () => {
+    if (this.props.onPrevMonth) {
+      return this.props.onPrevMonth();
+    }
+
+    this.props.onChangeMonth(this.props.date.clone().subtract(1, "months"));
+  };
+
+  render() {
+    const {
+      date,
+      weekOffset,
+      renderDay,
+      renderHeader,
+      onPickDate,
+      contentClassName,
+      containerClassName
+    } = this.props;
+
+    return (
+      <div className={cx("Calendar", containerClassName)}>
+        {renderHeader({
+          date,
+          onPrevMonth: this.handlePrevMonth,
+          onNextMonth: this.handleNextMonth
+        })}
+        <div className={cx("Calendar-grid", contentClassName)}>
+          {createDateObjects(date, weekOffset).map((day, i) =>
+            renderDay({ ...day, onPickDate })
+          )}
+        </div>
+      </div>
+    );
+  }
+}
